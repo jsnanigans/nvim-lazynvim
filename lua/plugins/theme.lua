50a4073743a096e03baa4d60ece5494f9202dcd5
Brendan Mullins <brendan.mullins@9am.health>
2023-12-07 21:35:34 +0100

    config

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,18 @@
+return {
+  -- "folke/tokyonight.nvim",
+  -- opts = {
+  --   transparent = true,
+  --   styles = {
+  --     sidebars = "transparent",
+  --     floats = "transparent",
+  --   },
+  -- },
+
+  -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox",
+  --   },
+  -- },
+}
